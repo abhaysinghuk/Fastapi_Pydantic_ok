6e0b4cbea5780e11ecc79be9b9f3fc4c173bd8fe
Abhay Singh <abhaysingh.cafo10@gmail.com>
2026-04-28 18:00:22 +0530

    update

---
+++ Pydantic/6_Nested_models.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+
+class Address(BaseModel):
+
+    city: str
+    state: str
+    country: str
+
+class Patient(BaseModel):
+
+    name: str
+    age: int
+    gender: str
+    address: Address
+
+address_dict = {'city': 'New York', 'state': 'NY', 'country': 'USA'}
+
+address1 = Address(**address_dict)
+
+patient_dict = {
+    'name': 'John Doe',
+    'age': 30,
+    'gender': 'male',
+    'address': address1
+}
+
+patient1 = Patient(**patient_dict)
+
+print(patient1)
+print(patient1.address.city)
+print(patient1.address.state)
+print(patient1.address.country)
